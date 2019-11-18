1c54c1f0d7737c5248aa88a4f788953000d95af3
allenyh <bicycleboy323@gmail.com>
2019-11-18 02:11:39 +0000

    first commit

---
+++ src/joint_logger.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import rospy
+import socket
+from sensor_msgs.msg import JointState
+from yumipy.yumi_constants import YuMiConstants as YMC
+
+
+class JointLogger:
+    def __init__(self):
+        self.joint_log_pub = rospy.Publisher('/joint_states', JointState, queue_size=1)
+        self.ip = YMC.IP
+        self.left_arm_port = YMC.PORTS['left']["states"]
+        self.right_arm_port = YMC.PORTS['right']['states']
+        self.left_arm_name = ["gripper_l_joint", "yumi_joint_1_l", "yumi_joint_2_l", "yumi_joint_3_l",
+                              "yumi_joint_4_l", "yumi_joint_5_l", "yumi_joint_6_l", "yumi_joint_7_l"]
+        self.right_arm_name = ["gripper_r_joint", "yumi_joint_1_r", "yumi_joint_2_r", "yumi_joint_3_r",
+                               "yumi_joint_4_r", "yumi_joint_5_r", "yumi_joint_6_r", "yumi_joint_7_r"]
+        self.left_arm_socket = socket.socket()
+        self.right_arm_socket = socket.socket()
+        self.left_arm_socket.connect((self.ip, self.left_arm_port))
+        self.right_arm_socket.connect((self.ip, self.right_arm_port))
+        self.timer = rospy.Timer(rospy.Duration(0.003), self.logCallBack)
+        self.joints = list()
+
+    def logCallBack(self, event):
+        js = JointState()
+        js.header.frame_id = 'map'
+        js.header.stamp = rospy.Time.now()
+        js.name = self.left_arm_name
+        msgs = self.left_arm_socket.recv(100)
+        msgs_list = self.process(msgs)
+        if len(msgs_list) == 0:
+            return
+        self.joints = list()
+        for i in range(len(msgs_list)):
+            if i == 0:
+                self.joints.append(float(msgs_list[i]))
+            else:
+                self.joints.append(float(msgs_list[i]) * 0.017453292)  # convert to rad
+        js.position = self.joints
+        self.joint_log_pub.publish(js)
+
+        js.header.stamp = rospy.Time.now()
+        js.name = self.right_arm_name
+        msgs = self.right_arm_socket.recv(100)
+        msgs_list = self.process(msgs)
+        if len(msgs_list) == 0:
+            return
+        self.joints = list()
+        for i in range(len(msgs_list)):
+            if i == 0:
+                self.joints.append(float(msgs_list[i]))
+            else:
+                self.joints.append(float(msgs_list[i]) * 0.017453292)  # convert to rad
+        js.position = self.joints
+        self.joint_log_pub.publish(js)
+
+    def process(self, msgs):
+        index_pound = msgs.find('#')
+        if index_pound == -1:
+            return list()
+        msgs = msgs[index_pound + 1:]
+        index_exclam = msgs.find('!')
+        if index_exclam == -1:
+            return list()
+        msgs = msgs[:index_exclam]
+        msgs_list = msgs.split()
+        if len(msgs_list) != 8:
+            return list()
+        return msgs_list
+
+
+if __name__ == '__main__':
+    rospy.init_node('JointLogger')
+    node = JointLogger()
+    rospy.spin()
---
+++ src/service_bridge.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+import rospy
+from yumipy import YuMiRobot
+from yumipy.yumi_util import message_to_pose, message_to_state
+from yumipy_service_bridge.srv import Trigger, TriggerResponse, \
+    GetPose, GetPoseResponse, GetJoint, GetJointResponse, GetGripper, GetGripperResponse
+
+
+class ServiceBridge:
+    def __init__(self):
+        self.ns = rospy.get_namespace()
+        robot = YuMiRobot(arm_type='remote')
+        self.left_arm = robot.left
+        self.right_arm = robot.right
+        self._create_services()
+        rospy.loginfo("[ServiceBridge] Node is up!")
+
+    def _create_services(self):
+        rospy.Service(self.ns + "goto_pose", GPose, self.goto_pose_cb)
+        rospy.Service(self.ns + "goto_joints", GJoint, self.goto_joints_cb)
+        rospy.Service(self.ns + "close_gripper", Trigger, self.close_gripper_cb)
+        rospy.Service(self.ns + "open_gripper", Trigger, self.open_gripper_cb)
+        rospy.Service(self.ns + "move_gripper", GGripper, self.move_gripper_cb)
+        rospy.Service(self.ns + "get_gripper_width", Trigger, self.get_gripper_width)
+        #rospy.Service(self.ns + "set_speed", , self.set_speed_cb)
+        #rospy.Service(self.ns + "set_zone", , self.set_zone_cb)
+        rospy.Service(self.ns + "reset_home", Trigger, self.reset_home_cb)
+        rospy.Service(self.ns + "calibrate_gripper", Trigger, self.calibrate_gripper_cb)
+        rospy.Service(self.ns + "turn_on_suction", Trigger, self.turn_on_suction_cb)
+        rospy.Service(self.ns + "turn_off_suction", Trigger, self.turn_off_suction_cb)
+
+    def goto_pose_cb(self, req):
+        response = GPoseResponse()
+        response.success = False
+        if (req.arm != 'left' and req.arm != 'right') or len(req.position) != 3 or len(req.quat) != 4:
+            return response
+        pose = message_to_pose(req.position + req.quat)
+        if req.arm == 'left':
+            if self.left_arm.is_pose_reachable(pose):
+                self.left_arm.goto_pose(pose)
+        else:
+            if self.right_arm.is_pose_reachable(pose):
+                self.right_arm.goto_pose(pose)
+        response.success = True
+        return response
+
+    def goto_joints_cb(self, req):
+        response = GJointResponse()
+        response.success = False
+        if (req.arm != 'left' and req.arm != 'right') or len(req.joint) != 7:
+            return response
+        state = message_to_state(req.joint)
+        if req.arm == 'left':
+            self.left_arm.goto_pose(state)
+        else:
+            self.right_arm.goto_pose(state)
+        response.success = True
+        return response
+
+    def close_gripper_cb(self, req):
+        response = TriggerResponse()
+        response.success = True
+        if req.arm == 'left':
+            self.left_arm.close_gripper()
+        elif req.arm == 'right':
+            self.right_arm.close_gripper()
+        else:
+            rospy.logerr("[Close Gripper] No arm named {}".format(req.arm))
+            response.success = False
+            response.message = "No arm named {}".format(req.arm)
+            return response
+        response.message = 'Successfully close {} gripper'.format(req.arm)
+        return response
+
+    def open_gripper_cb(self, req):
+        response = TriggerResponse()
+        response.success = True
+        if req.arm == 'left':
+            self.left_arm.open_gripper()
+        elif req.arm == 'right':
+            self.right_arm.open_gripper()
+        else:
+            rospy.logerr("[Open Gripper] No arm named {}".format(req.arm))
+            response.success = False
+            response.message = "No arm named {}".format(req.arm)
+            return response
+        response.message = 'Successfully open {} gripper'.format(req.arm)
+        return response
+
+    def move_gripper_cb(self, req):
+        response = GGripperResponse()
+        response.success = False
+        arm = None
+        if req.arm == 'left':
+            arm = self.left_arm
+        elif req.arm == 'right':
+            arm = self.right_arm
+        if arm is not None:
+            if req.width < 0:
+                arm.close_gripper()
+            elif req.width >= 0.025:
+                arm.open_gripper()
+            else:
+                arm.move_gripper(req.width)
+            response.success = True
+
+        return response
+
+    # def set_speed_cb(self, req):
+    #     # TODO write
+    #
+    # def set_zone_cb(self, req):
+    #     # TODO write
+
+    def get_gripper_width(self, req):
+        response = TriggerResponse()
+        response.success = True
+        if req.arm == 'left':
+            width = self.left_arm.get_gripper_width()
+        elif req.arm == 'right':
+            width = self.right_arm.get_gripper_width()
+        else:
+            rospy.logerr("[Get Gripper Width] No arm named {}".format(req.arm))
+            response.success = False
+            response.message = "No arm named {}".format(req.arm)
+            return response
+        response.message = str(width)
+        return response
+
+    def reset_home_cb(self, req):
+        response = TriggerResponse()
+        response.success = True
+        if req.arm == 'left':
+            self.left_arm.reset_home()
+        elif req.arm == 'right':
+            self.right_arm.reset_home()
+        else:
+            rospy.logerr("[Reset Home] No arm named {}".format(req.arm))
+            response.success = False
+            response.message = "No arm named {}".format(req.arm)
+            return response
+        response.message = 'Successfully reset {} arm to home'.format(req.arm)
+        return response
+
+    def calibrate_gripper_cb(self, req):
+        response = TriggerResponse()
+        response.success = True
+        if req.arm == 'left':
+            self.left_arm.calibrate_gripper()
+        elif req.arm == 'right':
+            self.right_arm.calibrate_gripper()
+        else:
+            rospy.logerr("[Reset Home] No arm named {}".format(req.arm))
+            response.success = False
+            response.message = "No arm named {}".format(req.arm)
+            return response
+        response.message = 'Successfully calibrate {} gripper'.format(req.arm)
+        return response
+
+    def turn_on_suction_cb(self, req):
+        response = TriggerResponse()
+        response.success = True
+        if req.arm == 'left':
+            self.left_arm.turn_on_suction()
+        elif req.arm == 'right':
+            self.right_arm.turn_on_suction()
+        else:
+            rospy.logerr("[Turn On Suction] No arm named {}".format(req.arm))
+            response.success = False
+            response.message = "No arm named {}".format(req.arm)
+            return response
+        response.message = 'Successfully Turn on {} suction'.format(req.arm)
+        return response
+
+    def turn_off_suction_cb(self, req):
+        response = TriggerResponse()
+        response.success = True
+        if req.arm == 'left':
+            self.left_arm.turn_off_suction()
+        elif req.arm == 'right':
+            self.right_arm.turn_off_suction()
+        else:
+            rospy.logerr("[Turn Off Suction] No arm named {}".format(req.arm))
+            response.success = False
+            response.message = "No arm named {}".format(req.arm)
+            return response
+        response.message = 'Successfully Turn off {} suction'.format(req.arm)
+        return response
+
+
+if __name__ == '__main__':
+    rospy.init_node('yumi_service_bridge_node')
+    node = ServiceBridge()
+    rospy.spin()
